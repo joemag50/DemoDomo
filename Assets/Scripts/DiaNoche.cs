630bc61c572d7da89507ecd541f5304dc4bbb6b2
José Carlos Gamboa Esparza <greengamboa@gmail.com>
2018-05-02 20:09:29 -0500

    Se agrego script para el sol

---
+++ Assets/Scripts/DiaNoche.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiaNoche : MonoBehaviour
+{
+    public float ciclo;
+
+    Light luzSol;
+    float tiempo;
+    bool AmanAnoch; //Amanecer = true, Anochecer = false
+
+	// Use this for initialization
+	void Start ()
+    {
+        tiempo = ciclo;
+        luzSol = GetComponent<Light>();
+        AmanAnoch = false; 
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (AmanAnoch)
+            tiempo += Time.deltaTime;
+        else
+            tiempo -= Time.deltaTime;
+
+        if (tiempo <= 0 || tiempo >= ciclo)
+            AmanAnoch = !AmanAnoch;
+
+        luzSol.intensity = tiempo / ciclo;
+    }
+}
---
+++ Assets/Scripts/MovimientoCamara.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour
+{
+    public GameObject pos1, pos2, pos3, pos4, Camara;
+    int cam;
+	// Use this for initialization
+	void Start ()
+    {
+        cam = 0;
+        OtraCamara();
+	}
+
+    public void OtraCamara()
+    {
+        cam++;
+        switch (cam)
+        {
+            case 1:
+                Camara.transform.position = pos1.transform.position;
+                Camara.transform.rotation = Quaternion.Euler(new Vector3(45, -45, 0));
+                break;
+            case 2:
+                Camara.transform.position = pos2.transform.position;
+                Camara.transform.rotation = Quaternion.Euler(new Vector3(45, -135, 0));
+                break;
+            case 3:
+                Camara.transform.position = pos3.transform.position;
+                Camara.transform.rotation = Quaternion.Euler(new Vector3(45, -225, 0));
+                break;
+            case 4:
+                Camara.transform.position = pos4.transform.position;
+                Camara.transform.rotation = Quaternion.Euler(new Vector3(45, -315, 0));
+                cam = 0;
+                break;
+            default:
+                break;
+        }
+    }
+}
