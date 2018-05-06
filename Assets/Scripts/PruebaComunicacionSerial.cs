8ff5f05fbc457bdbcb63652c956544bd244901c3
José Carlos Gamboa Esparza <greengamboa@gmail.com>
2018-05-06 11:31:46 -0500

    Se agrega archivo de prueba para comunicacion del serial

---
+++ Assets/Scripts/PruebaComunicacionSerial.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO.Ports;
+
+public class PruebaComunicacionSerial : MonoBehaviour
+{
+    SerialPort seri = new SerialPort("COM3", 9600);
+    float ciclo = 10;
+    float tiempo;
+    bool idaRegreso;
+
+    private void Awake()
+    {
+        
+    }
+    // Use this for initialization
+    void Start () {
+        seri.Open();
+        tiempo = ciclo;
+        idaRegreso = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (idaRegreso)
+        {
+            tiempo += Time.deltaTime;
+            seri.WriteLine("0");
+        }
+        else
+        {
+            tiempo -= Time.deltaTime;
+            seri.WriteLine("1");
+        }
+
+        if (tiempo <= 0 || tiempo >= ciclo)
+        {
+            idaRegreso = !idaRegreso;
+            seri.BaseStream.Flush();
+        }
+        Debug.Log(tiempo);
+
+	}
+}
